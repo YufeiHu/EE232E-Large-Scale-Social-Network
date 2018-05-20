822ed61460a6ab2bfdf38ceb2b82cd6883fe08e7
Yufei Hu <yufeihu@g.ucla.edu>
2018-05-20 14:16:10 -0700

    Add files via upload

---
+++ project3/part3/env4rl.py
@@ -0,0 +1,243 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+
+
+class MDPenv(object):
+    def __init__(self, greedy_prob=0.1, discount_factor=0.8, reward_function=None):
+        # space map
+        #self.space_idx = np.zeros([10, 10])
+        self.space = np.zeros([10, 10])
+        # action set (up, down, left, right)
+        self.action_set = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
+        # epsilon-greedy
+        self.greedy_prob = greedy_prob
+        # discount factor
+        self.discount_factor = discount_factor
+        # reward function
+        self.reward_function = reward_function
+        # policy
+        self.policy = np.array([[None] * 10] * 10)
+        # arrow set
+        self.mark_set = ['\u2191', '\u2193', '\u2190', '\u2192']
+
+
+    def draw_rewardmap(self):
+        ax1 = seaborn.heatmap(self.reward_function, annot=True, linecolor='black', linewidth=1, cmap='viridis')
+        plt.show()
+
+
+    def draw_map(self):
+        marks = np.array([[''] * 10] * 10)
+        mark_set = self.mark_set
+        for i in range(10):
+            for j in range(10):
+                if list(self.policy[i][j]) == [-1, 0]:
+                    marks[i][j] = mark_set[0]
+                elif list(self.policy[i][j]) == [1, 0]:
+                    marks[i][j] = mark_set[1]
+                elif list(self.policy[i][j]) == [0, -1]:
+                    marks[i][j] = mark_set[2]
+                elif list(self.policy[i][j]) == [0, 1]:
+                    marks[i][j] = mark_set[3]
+
+        ax1 = seaborn.heatmap(self.space, annot=True, linecolor='black', linewidth=1, cmap='viridis')
+        plt.show()
+        ax2 = seaborn.heatmap(self.space, annot=marks, fmt='s', linecolor='black', linewidth=1, cmap='viridis')
+        plt.show()
+
+
+    def transition_prob(self, current_state, action, next_state):
+        # impossible leap
+        if ((current_state[0] == next_state[0] and current_state[1] == next_state[1]) or
+           (current_state[0] == next_state[0] + 1 and current_state[1] == next_state[1]) or
+           (current_state[0] == next_state[0] - 1 and current_state[1] == next_state[1]) or
+           (current_state[0] == next_state[0] and current_state[1] == next_state[1] + 1) or
+           (current_state[0] == next_state[0] and current_state[1] == next_state[1] - 1)) == False:
+            return 0.0
+        
+        # intended state
+        intended_state = current_state + action
+
+        # normal case:
+        if current_state[0] > 0 and current_state[0] < 9 and current_state[1] > 0 and current_state[1] < 9:
+            if list(next_state) == list(current_state):
+                return 0
+            if list(next_state) == list(intended_state):
+                return (1 - self.greedy_prob + self.greedy_prob / 4)
+            else:
+                return self.greedy_prob / 4
+
+        # corner 1:
+        elif list(current_state) == [0, 0]:
+            if intended_state[0] < 0 or intended_state[1] < 0:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4 + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(current_state):
+                    return self.greedy_prob / 4 + self.greedy_prob / 4
+                elif list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+        # corner 2:
+        elif list(current_state) == [0, 9]:
+            if intended_state[0] < 0 or intended_state[1] > 9:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4 + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(current_state):
+                    return self.greedy_prob / 4 + self.greedy_prob / 4
+                elif list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+        # corner 3:
+        elif list(current_state) == [9, 0]:
+            if intended_state[0] > 9 or intended_state[1] < 0:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4 + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(current_state):
+                    return self.greedy_prob / 4 + self.greedy_prob / 4
+                elif list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+
+        # corner 4:
+        elif list(current_state) == [9, 9]:
+            if intended_state[0] > 9 or intended_state[1] > 9:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4 + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(current_state):
+                    return self.greedy_prob / 4 + self.greedy_prob / 4
+                elif list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+        # edge 1:
+        elif current_state[0] == 0 and list(current_state) != [0, 0] and list(current_state) != [0, 9]:
+            if intended_state[0] < 0:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+        # edge 2:
+        elif current_state[0] == 9 and list(current_state) != [9, 0] and list(current_state) != [9, 9]:
+            if intended_state[0] > 9:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+        # edge 3:
+        elif current_state[1] == 0 and list(current_state) != [0, 0] and list(current_state) != [9, 0]:
+            if intended_state[1] < 0:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+
+        # edge 4:
+        elif current_state[1] == 9 and list(current_state) != [0, 9] and list(current_state) != [9, 9]:
+            if intended_state[1] > 9:
+                if list(next_state) == list(current_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+            else:
+                if list(next_state) == list(intended_state):
+                    return 1 - self.greedy_prob + self.greedy_prob / 4
+                else:
+                    return self.greedy_prob / 4
+        
+        # out of bound:
+        else:
+            return 0.0
+
+
+    def optimal_state_value(self, current_state):
+        # initial
+        current_value = -float('inf')
+        current_policy = np.array([0, 0])
+        # computation
+        for action in self.action_set:
+            value_tmp = 0
+            for move in np.array([[-1,0], [1,0], [0,-1], [0,1], [0,0]]):
+                next_state = current_state + move
+                if next_state[0] < 0 or next_state[0] > 9 or next_state[1] < 0 or next_state[1] > 9:
+                    continue 
+                prob = self.transition_prob(current_state, action, next_state)
+                value_tmp += prob * (self.reward_function[next_state[0]][next_state[1]] + self.discount_factor * self.space[next_state[0]][next_state[1]])
+            if value_tmp > current_value:
+                current_policy = action
+                current_value = value_tmp
+        return current_value, current_policy
+
+    
+    def one2two(self, idx):
+        y = int(idx % 10)
+        x = int(idx / 10)
+        return np.array([y, x])
+        
+        
+    def compute_probability_matrix(self):
+        optimal_action = self.policy
+        Pa1 = np.zeros((100, 100))
+        Pa = np.zeros((100, 100, 3))
+        for idx0 in range(100):
+            current_state = self.one2two(idx0)
+            a1 = optimal_action[current_state[0], current_state[1]]
+            for idx1 in range(100):
+                next_state = self.one2two(idx1)
+                Pa1[idx0, idx1] = self.transition_prob(current_state, a1, next_state)
+                idx2 = 0
+                for action in self.action_set:
+                    if action[0] != a1[0] and action[1] != a1[1]:
+                        Pa[idx0, idx1, idx2] = self.transition_prob(current_state, action, next_state)
+                        idx2 += 1
+        return Pa1, Pa
+
+
+    def value_iter(self, epsilon):
+        self.space = np.zeros([10, 10])
+        delta = float('inf')
+        while delta > epsilon:
+            delta = 0
+            for i in range(10):
+                for j in range(10):
+                    curr = self.space[i][j]
+                    self.space[i][j], self.policy[i][j] = self.optimal_state_value(np.array([i, j]))
+                    delta = max(delta, abs(curr - self.space[i][j]))
+        return self.space, self.policy
+
---
+++ project3/part3/main.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 17:57:21 2018
+@author: Yufei Hu
+"""
+
+
+from cvxopt import matrix, solvers
+from env4rl import MDPenv
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+
+
+def compute_reward(lamb, gamma, Pa1, Pa):
+    
+    C = [1] * 100
+    C.extend([-lamb] * 100)
+    C.extend([0] * 100)
+    C = np.array(C)
+    C = C.reshape((300, 1))
+    
+    I = np.identity(100)
+    M = I - gamma * Pa1
+    M = np.linalg.inv(M)
+    
+    D0 = np.concatenate((np.identity(100), np.zeros((100, 100))), axis=1)
+    D0 = np.concatenate((D0, D0, D0), axis=0)
+    D0_R = list()
+    for i in range(Pa.shape[2]):
+        for j in range(Pa.shape[0]):
+            vec_left = Pa1[j] - Pa[j, :, i]
+            vec_left = vec_left.reshape((1, vec_left.shape[0]))
+            vec_ans = -vec_left.dot(M)
+            D0_R.append(vec_ans[0])
+    D0_R = np.array(D0_R)
+    D0 = np.concatenate((D0, D0_R), axis=1)
+    
+    D1 = np.zeros((300, 200))
+    D1_R0 = -(Pa1 - Pa[:, :, 0])
+    D1_R0 = D1_R0.dot(M)
+    D1_R1 = -(Pa1 - Pa[:, :, 1])
+    D1_R1 = D1_R1.dot(M)
+    D1_R2 = -(Pa1 - Pa[:, :, 2])
+    D1_R2 = D1_R2.dot(M)
+    D1_R = np.concatenate((D1_R0, D1_R1, D1_R2), axis=0)
+    D1 = np.concatenate((D1, D1_R), axis=1)
+    
+    D2_0 = np.concatenate((np.zeros((100, 100)), -np.identity(100), -np.identity(100)), axis=1)
+    D2_1 = np.concatenate((np.zeros((100, 100)), -np.identity(100), np.identity(100)), axis=1)
+    D2 = np.concatenate((D2_0, D2_1), axis=0)
+    
+    D3_0 = np.concatenate((np.zeros((100, 200)), np.identity(100)), axis=1)
+    D3_1 = np.concatenate((np.zeros((100, 200)), -np.identity(100)), axis=1)
+    D3 = np.concatenate((D3_0, D3_1), axis=0)
+    
+    D = np.concatenate((D0, D1, D2, D3), axis=0)
+    
+    b = np.zeros((1, 1000))
+    b[0, 800:] = 1
+    b = np.array(b)
+    
+    C_cvx = matrix(-C)
+    A_cvx = matrix(D)
+    b_cvx = matrix(b.T)
+    sol = solvers.lp(C_cvx, A_cvx, b_cvx)
+    ans = list(sol['x'])
+    R_hat = np.array(ans[200:])
+    R_hat = R_hat.reshape((10, 10))
+    
+    return R_hat
+    
+
+rf1 = np.array([[ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.]])
+
+
+rf2 = np.array([[ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  -100.0,  -100.0,  -100.0,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  -100.0,  0.,  -100.0,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  -100.0,  0.,  -100.0,  -100.0,  -100.0,  0.],
+                [ 0.,  0.,  0.,  0.,  -100.0,  0.,  0.,  0.,  -100.0,  0.],
+                [ 0.,  0.,  0.,  0.,  -100.0,  0.,  0.,  0.,  -100.0,  0.],
+                [ 0.,  0.,  0.,  0.,  -100.0,  0.,  0.,  0.,  -100.0,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  -100.0,  -100.0,  -100.0,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  -100.0,  0.,  0.,  0.],
+                [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  10.]])
+
+
+lamb_list = np.linspace(0, 5, 50)
+gamma = 0.8
+
+
+env1 = MDPenv(reward_function=rf1)
+_, policy_optimal = env1.value_iter(epsilon=0.01)
+Pa1, Pa = env1.compute_probability_matrix()
+
+
+acc_total = list()
+for lamb in lamb_list:
+    reward_cur = compute_reward(lamb, gamma, Pa1, Pa)
+    env_cur = MDPenv(reward_function=reward_cur)
+    _, policy_cur = env_cur.value_iter(epsilon=0.01)
+    compare = 0
+    for i in range(10):
+        for j in range(10):
+            if list(policy_cur[i, j]) == list(policy_optimal[i, j]):
+                compare += 1
+    acc_cur = compare / 100.0
+    acc_total.append(acc_cur)
+plt.figure()
+plt.plot(lamb_list, acc_total)
+plt.show()

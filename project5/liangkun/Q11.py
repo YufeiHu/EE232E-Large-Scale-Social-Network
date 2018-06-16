963b779c133a579498aa21ccd7a999c10f2a38e9
LiangkunZ <liangkunzhao715@gmail.com>
2018-06-15 19:44:11 -0700

    Add files via upload

--- project5/liangkun/Q11.py
+++ project5/liangkun/Q11.py
@@ -22,13 +22,16 @@ file_uberGraph_info.close()
 locations_np = np.array(locations)
 tri = Delaunay(locations_np)
 
-# import matplotlib.pyplot as plt
-# plt.triplot(locations[:,0], locations[:,1], tri.simplices.copy())
-# plt.plot(locations[:,0], locations[:,1], 'o')
-# plt.show()
+import matplotlib.pyplot as plt
+print(locations_np)
+plt.triplot(locations_np[:,0], locations_np[:,1], tri.simplices.copy())
+plt.plot(locations_np[:,0], locations_np[:,1], 'o')
+plt.show()
 
-file_Delaunay = open("./data/uber/Delaunay.txt", "w")
-for array in tri.simplices:
-    file_Delaunay.write("{:d}\t\t{:d}\t\t{:d}\n".format(int(id_list[array[0]]), int(id_list[array[1]]), int(id_list[array[2]])))
+# file_Delaunay = open("./data/uber/Delaunay.txt", "w")
+# for array in tri.simplices:
+#     file_Delaunay.write("{:d}\t\t{:d}\t\t{:d}\n".format(int(id_list[array[0]]), int(id_list[array[1]]), 1))
+#     file_Delaunay.write("{:d}\t\t{:d}\t\t{:d}\n".format(int(id_list[array[1]]), int(id_list[array[2]]), 1))
+#     file_Delaunay.write("{:d}\t\t{:d}\t\t{:d}\n".format(int(id_list[array[0]]), int(id_list[array[2]]), 1))
 
-file_Delaunay.close()
+# file_Delaunay.close()
